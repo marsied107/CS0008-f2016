3b9aab14176a96563220af9ce765a5a8ce320bbf
marielledavis <mjd120@pitt.edu>
2016-09-15 15:48:20 -0400

    Chapter 3 exercises 1-4 & 9

---
+++ Ch3-Ex/Ch3-Ex9.py
@@ -0,0 +1,28 @@
+#Asks the user for a number between 0 and 36
+number = (input('Enter a number 0-36: '))
+
+#Makes sure the number is an integer
+number = int(number)
+
+#tets if number is between 0 and 36
+if number < 0 or number > 36:
+    print ('Give me a number between 0 and 36')
+
+if number == 0:
+    color = "green"
+elif (number >= 1 and number <= 10) \
+        or \
+     (number >= 19 and number <= 28):
+    if number %2 == 0:
+        color = 'black'
+    else:
+        color = 'red'
+elif (number >= 11 and number <= 18) \
+        or \
+     (number >= 29 and number <= 36):
+    if number %2 == 0:
+        color = 'red'
+    else:
+        color = 'black'
+
+print('Your color is', (color))
