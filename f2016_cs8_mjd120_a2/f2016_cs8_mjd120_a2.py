cde66d4dcd7478f519994e79770b4852fbb1752d
nitrosx (Max Novelli) <man8@pitt.edu>
2016-11-27 13:13:14 -0500

    fixes and comments on assignment #2

--- f2016_cs8_mjd120_a2/f2016_cs8_mjd120_a2.py
+++ f2016_cs8_mjd120_a2/f2016_cs8_mjd120_a2.py
@@ -1,8 +1,14 @@
-#Creates a loop to read each line of the data file
-#Converts the string into an array
-#Adds one for each line and adds the data together
-#Removes the last line
-#Returns the total number of lines and total amount of data
+#
+# MN: header with user, instructor and course info is missing
+#
+# MN: there is not enough comments on each block of code
+#
+
+# Creates a loop to read each line of the data file
+# Converts the string into an array
+# Adds one for each line and adds the data together
+# Removes the last line
+# Returns the total number of lines and total amount of data
 def processFile(fh):
     partial_distance = 0
     line_number = 0
@@ -14,11 +20,11 @@ def processFile(fh):
         partial_distance += dist
     return line_number,partial_distance
 
-#Prints out the info that is returned from the function
-#Formats the key at n-length characters
-#Formats the the string so that it is 20 spaces long
-#Formats the integer so that it is 10 spaces long
-#Formats the float so that it is 10 spaces and 3 decimal spaces long
+# Prints out the info that is returned from the function
+# Formats the key at n-length characters
+# Formats the the string so that it is 20 spaces long
+# Formats the integer so that it is 10 spaces long
+# Formats the float so that it is 10 spaces and 3 decimal spaces long
 def printKV(key, value, klen = 0):
     if isinstance(value, str):
         print key + " " + format(value, '20,.f')
@@ -27,9 +33,9 @@ def printKV(key, value, klen = 0):
     elif isinstance(value, float):
         print key + " " + format(value, '10,.3f')
 
-#Sets total distance and total number = to 0
-#Creates the loop to open the function, return, and quit
-#Initalizes the printKV function
+# Sets total distance and total number = to 0
+# Creates the loop to open the function, return, and quit
+# Initalizes the printKV function
 total_distance = 0
 total_number = 0
 name = input('File to be read: ')
